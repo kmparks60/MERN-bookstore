389f841fd19c5bc436a2f7b8eee91f7dd10fef88
Mason Parks <kmparks60@gmail.com>
2024-05-29 18:08:15 -0400

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://root:root@mern-book-store.sde8lxs.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN-Book-Store'
